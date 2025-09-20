3adbd4cb978dc077d1092288441cc56322767ce5
PriyanshuA23 <pk13091976@gmail.com>
2025-09-20 13:25:48 +0530

    Created js file to start solving the problem

---
+++ lion_hunt_problem/lion_hunt.js
@@ -0,0 +1,15 @@
+const testCase1 = "LZ";
+const testCase2 = "Z L";
+const testCase3 = "L     Z";
+const testCase4 = "L     L";
+const testCase5 = "Z   Z   Z";
+const testCase6 = "L  ZL Z";
+
+const testCaseToUse = testCase1;
+
+const lion = "L";
+const Zebra = "Z";
+
+let shortestSpace = 0;
+
+console.log("Input:",testCase1," Output:",shortestSpace);
