6151f92e05ad8c259b4842ee3c28904354f0943d
PriyanshuA23 <pk13091976@gmail.com>
2025-09-20 14:30:45 +0530

    Updated file for particular input

--- lion_hunt_problem/finding_animal.js
+++ lion_hunt_problem/finding_animal.js
@@ -1,4 +1,4 @@
-const testCase = "Z LZ";
+const testCase = " Z LZ";
 
 const lion = "L";
 const zebra = "Z";
@@ -7,6 +7,34 @@ let animalFound = "No Animal";
 
 if((testCase[0] === lion) || (testCase[0] === zebra)) {
     animalFound = (testCase[0] === lion) ? "L" : "Z";
+}
+
+console.log(animalFound,"is found");
+
+animalFound = "No Animal";
+if(testCase[1] === lion || testCase[1] === zebra) {
+    animalFound = testCase[1] === lion ? "L" : "Z";
+}
+
+console.log(animalFound,"is found");
+
+animalFound = "No Animal";
+if(testCase[2] === lion || testCase[2] === zebra) {
+    animalFound = testCase[2] === lion ? "L" : "Z";
+}
+
+console.log(animalFound,"is found");
+
+animalFound = "No Animal";
+if(testCase[3] === lion || testCase[3] === zebra) {
+    animalFound = testCase[3] === lion ? "L" : "Z";
+}
+
+console.log(animalFound,"is found");
+
+animalFound = "No Animal";
+if(testCase[4] === lion || testCase[4] === zebra) {
+    animalFound = testCase[4] === lion ? "L" : "Z";
 }
 
 console.log(animalFound,"is found");
