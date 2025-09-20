44ee47b63b576f07cef0a28086ed627c7e88ae59
PriyanshuA23 <pk13091976@gmail.com>
2025-09-20 10:38:17 +0530

    Create a js file to calculate factorial of a number

---
+++ factorial/calculate_factorial.js
@@ -0,0 +1,8 @@
+const calculateFactorialOf = 2;
+let numberToOperate = calculateFactorialOf;
+let resultOfFactorial = 0;
+
+resultOfFactorial = numberToOperate * (numberToOperate-1);
+
+console.log("factorial of",calculateFactorialOf,"is",resultOfFactorial);
+
